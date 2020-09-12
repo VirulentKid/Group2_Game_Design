6d51324733066ff4f04a7258126f3943ba37a742
Domnee <70956754+Domnee@users.noreply.github.com>
2020-09-12 14:09:33 -0400

    projectile arm control + basic map

---
+++ Group2_Game_project/Assets/Projectile.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    bool facingR = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //faceMouse();
+    }
+
+    
+
+    void faceMouse()
+    {
+        Vector3 mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        if(mouse.x > transform.position.x && !facingR)
+        {
+            transform.Rotate(0f, 180f, 0f);
+            facingR = true;
+        }
+        else if(mouse.x < transform.position.x && facingR)
+        {
+            transform.Rotate(0f, 180f, 0f);
+            facingR = false;
+        }
+    }
+}
---
+++ Group2_Game_project/Assets/projectileLauncher.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class projectileLauncher : MonoBehaviour
+{
+    bool facingR = true;
+    public Rigidbody2D player;
+    public GameObject myPlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        followPlayer();
+        follow();
+    }
+
+    //let arm follow the mouse and flip
+    void follow()
+    {
+        Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        difference.Normalize();
+        float z = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, z);
+        if(z < -90 || z > 90)
+        {
+            if(myPlayer.transform.eulerAngles.y == 0 || myPlayer.transform.eulerAngles.y == 180)
+                transform.localRotation = Quaternion.Euler(180, 0, -z);
+        }
+    }
+
+    //let the arm and gun follow the player
+    void followPlayer()
+    {
+        transform.position = player.position + new Vector2(-0.069f, 0.221f); //minus arm's coordinate by player's position
+    }
+}
